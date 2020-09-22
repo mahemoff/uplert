050f4a22e2f633a7057297fc48a86c76dd4188dc
Michael Mahemoff <michael@mahemoff.com>
2020-09-22 20:23:02 +1000

    factor out generic calls

--- uplert.js
+++ uplert.js
@@ -1,16 +1,33 @@
 const r2 = require("r2");
 const url = `https://api.up.com.au/api/v1/accounts/${process.env.UP_ACCOUNT_ID}`
-let headers = {'Authorization': process.env.UP_KEY}
 let minBalance = 200;
 
-const getData = async url => {
+//////////////////////////////////////////////////////////////////////////////
+// GENERIC HTTP
+//////////////////////////////////////////////////////////////////////////////
+
+const httpGet = async (url, headers) => {
   try {
     const response = await r2(url, {headers}).json;
-    let balance = response.data.attributes.balance.value;
-    console.log ( `Balance ${(balance < minBalance) ? 'low!' : 'OK'}` );
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-getData(url);
+//////////////////////////////////////////////////////////////////////////////
+// GENERIC UPBANK API
+//////////////////////////////////////////////////////////////////////////////
+
+const getUp = async (path) => {
+  const url = `https://api.up.com.au/api/v1/${path}`
+  const response = await httpGet(url, {'Authorization': process.env.UP_KEY});
+  let balance = response.attributes.balance.value;
+  console.log ( `Balance ${(balance < minBalance) ? 'low!' : 'OK'}` );
+};
+
+//////////////////////////////////////////////////////////////////////////////
+// PAYMENTS
+//////////////////////////////////////////////////////////////////////////////
+
+getUp(`/accounts/${process.env.UP_ACCOUNT_ID}`);
