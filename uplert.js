b1f303bc725d0a5653846156102cf193ba8cc8df
Michael Mahemoff <michael@mahemoff.com>
2020-09-29 23:54:53 +1000

    command-line args, accounts list

--- uplert.js
+++ uplert.js
@@ -3,7 +3,16 @@
 //////////////////////////////////////////////////////////////////////////////
 
 import config from 'config';
+import yargs from 'yargs'
+//import currencySymbol from 'currency-symbol';
+//import currency from 'currency.js';
+//import * as currency from 'currency.js';
+import { hideBin } from 'yargs/helpers'
 import * as alerter from './lib/app/alerter.mjs';
+import getSymbolFromCurrency from 'currency-symbol-map'
+
+
+import * as upBank from './lib/util/upbank.mjs';
 
 //////////////////////////////////////////////////////////////////////////////
 // I18N IMPORT
@@ -27,5 +36,30 @@ config.i18n = i18n;
 //////////////////////////////////////////////////////////////////////////////
 
 (async() => {
-  alerter.alertLowBalance(config);
+
+  const {argv} = yargs(hideBin(process.argv))
+    .command('alert', 'send alert if under balance', () => {}, async (argv) => {
+      alerter.alertLowBalance(config);
+    })
+    .command('summary [format]', 'summary of all accounts', (yargs) => {
+      yargs.positional('format', { describe: 'format to render', default: 'basic' })
+    }, async (argv) => {
+      const format = argv.format.toLowerCase();
+      const accounts = await upBank.getAccounts(config)
+      if (format=='basic') {
+        console.log(accounts.map( (account) => {
+          const attribs = account.attributes;
+          const symbol = getSymbolFromCurrency(attribs.balance.currencyCode);
+          return `${attribs.displayName}: ${attribs.balance.currencyCode} ${symbol}${attribs.balance.value} (${attribs.accountType.toLowerCase()})`
+        }).join("\n"));
+      } else if (format=='json') {
+        console.info(JSON.stringify(accounts, null, 2));
+      }
+    })
+    .demandCommand(1).argv
+
+  //console.log(argv);
+
+  //alerter.alertLowBalance(config);
+
 })();
